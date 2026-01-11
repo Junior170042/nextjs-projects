0649886dd77164399987e9aec16a2e86a35b4907
Junior170042 <vernardstverty@gmail.com>
2026-01-10 21:29:02 -0300

    fixing buggs

--- app/pages/NotFound.tsx
+++ app/pages/NotFound.tsx
@@ -1,6 +1,7 @@
-import { useNavigate } from "react-router-dom";
+"use client";
+import { useRouter } from "next/router";
 export default function NotFound() {
-    const navigate = useNavigate();
+    const router = useRouter();
     return (
         <div className="min-h-screen flex-col-center w-full">
             <img src="postOne.png" alt="postOne" className="w-32 h-32 filter drop-shadow-lg rounded-full invert-[100%]" />
@@ -8,7 +9,7 @@ export default function NotFound() {
                 The page you are looking for does not exist.
             </p>
             <button className="bg-indigo-600 text-white px-4 py-2 rounded-full hover:bg-indigo-700 transition-colors duration-200 mt-2 cursor-pointer"
-                onClick={() => navigate('/')}
+                onClick={() => router.push('/')}
             >
                 Go Home
             </button>
